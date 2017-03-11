4ed6f103a4669a8370ecc315077bc80465bc6062
Grzegorz Kotfis <Grzegorz Kotfis>
2017-03-11 17:14:25 +0100

    Migrate to .csproj VS2017, add publish to IIS script

--- src/Scripts/publish.bat
+++ src/Scripts/publish.bat
@@ -2,7 +2,7 @@ echo off
 %windir%\system32\inetsrv\appcmd stop site /site.name:Dogevents
 %windir%\system32\inetsrv\appcmd stop apppool /apppool.name:Dogevents
 
-dotnet publish %cd%\..\Web\Dogevents.Web\project.json -o %cd%\..\..\publish -c Release
+dotnet publish %cd%\..\Web\Dogevents.Web\Dogevents.Web.csproj -o %cd%\..\..\publish -c Release
 
 %windir%\system32\inetsrv\appcmd start site /site.name:Dogevents
 %windir%\system32\inetsrv\appcmd start apppool /apppool.name:Dogevents
