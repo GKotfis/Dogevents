34a4a33e26f40518d0277b81cbc32a01747a97ef
Grzegorz Kotfis <gkotfis@outlook.com>
2017-03-19 00:25:32 +0100

    change domain model, add url parser, test project

---
+++ src/Web/Dogevents.Core/Helpers/UrlParser.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dogevents.Core.Helpers
+{
+    public static class UrlParser
+    {
+        public static string GetEventId(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return string.Empty;
+
+            return Regex.Match(url, @"(?:events)?(?:/)?(?'id'\d+)").Groups["id"].Value;
+        }
+    }
+}
---
+++ src/Web/Dogevents.Web/Areas/Administration/Controllers/EventsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Dogevents.Core.Helpers;
+using Dogevents.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dogevents.Web.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    public class EventsController : Controller
+    {
+        private IEventsService _eventsService;
+        private IFacebookService _facebookService;
+
+        public EventsController(IEventsService eventsService, IFacebookService facebookService)
+        {
+            _eventsService = eventsService;
+            _facebookService = facebookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dogevents = await _eventsService.GetAll();
+            return View(dogevents);
+        }
+
+        public async Task<IActionResult> Add(string eventUrl)
+        {
+            var eventId = UrlParser.GetEventId(eventUrl);
+
+            await _facebookService.GetEventAsync(eventId)
+                                .ContinueWith(@event => _eventsService.Add(@event.Result));
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(long id)
+        {
+            await _eventsService.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
