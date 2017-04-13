899bce0552fddb9a118b049ad8e82d40fd70e2cb
Grzegorz Kotfis <gkotfis@outlook.com>
2017-04-13 10:59:00 +0200

    fix api url

--- src/Web/Dogevents.Web.Client/webpack.config.js
+++ src/Web/Dogevents.Web.Client/webpack.config.js
@@ -50,12 +50,14 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
- module.exports.plugins = (module.exports.plugins || []).concat([
+if (process.env.NODE_ENV === 'development') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         API_URL: '"http://localhost:5000/api"'
       }
     })])
+}
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
