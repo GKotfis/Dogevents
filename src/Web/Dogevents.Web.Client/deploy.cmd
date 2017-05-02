c6762c9132613557bc7af8c4817ca0aa84dc4bb4
Grzegorz Kotfis <gkotfis@outlook.com>
2017-05-02 12:44:52 +0200

    remove unused run build

--- src/Web/Dogevents.Web.Client/deploy.cmd
+++ src/Web/Dogevents.Web.Client/deploy.cmd
@@ -119,14 +119,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-:: 4. Run build
-IF EXIST "%DEPLOYMENT_TARGET%\webpack.config.js" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! run build
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
 
