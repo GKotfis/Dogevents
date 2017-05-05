587bb87d96569dbe956ebb6ce0d21a60ce6c4f2a
Grzegorz Kotfis <gkotfis@outlook.com>
2017-05-05 11:32:40 +0200

    Add sidenav with search

--- src/Web/Dogevents.Web.Client/src/App.vue
+++ src/Web/Dogevents.Web.Client/src/App.vue
@@ -1,16 +1,38 @@
 <template>
     <div class="app-viewport">
-        <md-toolbar class="top-fixed md-dense">
-            <i class="material-icons">event</i>
-            <h2 class="md-title" style="flex: 1" md-hide-xsmall>Dogevents</h2>
-            <md-button class="md-icon-button">
+        <md-toolbar class="md-dense">
+            <md-layout class="main-menu" md-flex="0" md-hide-small>
+                <i class="material-icons">event</i>
+                <h2 class="md-title" style="flex: 1" md-hide-xsmall>Dogevents</h2>
+            </md-layout>
+            <md-input-container style="flex: 1">
+                <label>Szukaj w okolicy</label>
+                <md-input v-model="initialValue"></md-input>
+            </md-input-container>
+            <md-button class="md-icon-button md-accent" @click.native="toggleSearchMenu">
                 <md-icon>search</md-icon>
             </md-button>
         </md-toolbar>
+        <md-sidenav class="md-right" ref="searchSideNav">
+            <md-toolbar>
+                <div class="md-toolbar-container">
+                    <h3 class="md-title">Szukaj wydarzeń</h3>
+                </div>
+            </md-toolbar>
+            <searchNav />
+            <md-button class="md-raised md-accent" @click.native="closeSearchSideNav">
+                <md-icon>close</md-icon>
+            </md-button>
+        </md-sidenav>
         <main>
             <popular />
             <justadded />
         </main>
+        <md-bottom-bar md-hide-large>
+            <md-bottom-bar-item md-icon="history">Recents</md-bottom-bar-item>
+            <md-bottom-bar-item md-icon="favorite" md-active>Favorites</md-bottom-bar-item>
+            <md-bottom-bar-item md-icon="near_me">Nearby</md-bottom-bar-item>
+        </md-bottom-bar>
     </div>
 </template>
 
@@ -19,11 +41,22 @@
     import VueMaterial from 'vue-material'
     import popular from './components/Popular.vue'
     import justadded from './components/JustAdded.vue'
+    import searchNav from './components/SearchNav.vue'
 
     export default {
         components: {
             popular,
-            justadded
+            justadded,
+            searchNav
+        },
+        methods: {
+            toggleSearchMenu() {
+                this.$refs.searchSideNav.toggle();
+            },
+            closeSearchSideNav() {
+                this.$refs.searchSideNav.close();
+            },
+
         }
     }
 
@@ -33,16 +66,5 @@
     .app-viewport {
         display: flex;
         flex-flow: column;
-    }
-    
-    .top-fixed {
-        position: fixed;
-        top: 0;
-        z-index: 1000;
-        width: 100%;
-    }
-    
-    main {
-        margin-top: 60px;
     }
 </style>
---
+++ src/Web/Dogevents.Web.Client/src/components/SearchNav.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <md-button-toggle md-single>
+            <md-layout>
+                <md-button >Seminaria</md-button>
+                <md-button>Sport</md-button>
+                <md-button>Warsztaty</md-button>
+                <md-button>Tropy</md-button>
+            </md-layout>
+        </md-button-toggle>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: 'searchNav',
+        data: () => ({
+        }),
+        mounted() {
+        },
+        components: {
+
+        }
+    }
+
+</script>
+<style>
+
+</style>
