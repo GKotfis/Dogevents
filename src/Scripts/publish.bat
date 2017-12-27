61ae3041d2ee66024eb83c4c881b1474df8aa284
Grzegorz Kotfis <gkotfis@outlook.com>
2017-12-27 13:01:14 +0100

    Fix publish script

--- src/Scripts/publish.bat
+++ src/Scripts/publish.bat
@@ -1,8 +1,8 @@
 echo off
-%windir%\system32\inetsrv\appcmd stop site /site.name:Dogevents
+%windir%\system32\inetsrv\appcmd stop site /site.name:Dogevents.Api
 %windir%\system32\inetsrv\appcmd stop apppool /apppool.name:Dogevents
 
-dotnet publish %cd%\..\Web\Dogevents.Web\Dogevents.Web.csproj -o %cd%\..\..\publish -c Release
+dotnet publish %cd%\..\Web\Dogevents.Web\Dogevents.Web.csproj -o %cd%\..\Web\Dogevents.Web\bin\Debug\Publish -c Debug
 
-%windir%\system32\inetsrv\appcmd start site /site.name:Dogevents
+%windir%\system32\inetsrv\appcmd start site /site.name:Dogevents.Api
 %windir%\system32\inetsrv\appcmd start apppool /apppool.name:Dogevents
