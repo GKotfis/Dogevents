561e1d7d27f1fe5b2b00d96fddb075aabd667faf
Grzegorz Kotfis <gkotfis@outlook.com>
2017-12-27 12:58:20 +0100

    Add lightweight model

---
+++ src/Web/Dogevents.Web/Models/EventCardHeaderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Dogevents.Core.Domain;
+
+namespace Dogevents.Web.Models
+{
+    public class EventCardHeaderViewModel : IViewEventModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public string Url { get => $"https://www.facebook.com/events/{Id}/"; }
+    }
+}
---
+++ src/Web/Dogevents.Web/Models/EventCardViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Dogevents.Core.Domain;
+using Newtonsoft.Json;
+
+namespace Dogevents.Web.Models
+{
+    public class EventCardViewModel : IViewEventModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        [JsonProperty("start_time")]
+        public DateTime StartTime { get; set; }
+
+        [JsonProperty("end_time")]
+        public DateTime EndTime { get; set; }
+
+        public string Url { get => $"https://www.facebook.com/events/{Id}/"; }
+
+        public string CoverUrl { get => Cover.source; }
+
+        public Cover Cover { get; set; }
+        public Place Place { get; set; }
+    }
+
+    public class Cover
+    {
+        public string source { get; set; }
+    }
+
+    public class Owner
+    {
+        public string Name { get; set; }
+    }
+
+    public class Place
+    {
+        public string Name { get; set; }
+        public Location Location { get; set; }
+    }
+
+    public class Location
+    {
+        public string City { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
